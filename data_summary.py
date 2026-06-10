7d74e21aa746d52248f8e36ea4952954088a8027
Taku-Sakaue <98134385+Taku-Sakaue@users.noreply.github.com>
2026-06-10 16:37:36 +0900

    Update data_summary.py

--- data_summary.py
+++ data_summary.py
@@ -10,5 +10,21 @@ st.title('参加者要約')
 # 参加者情報の読み取り
 indat = pd.read_excel(r'data/data.xlsx', skiprows=1, header=None)
 
-# OSS
-ossdat = indat[indat[6] == "製薬業界におけるR活用の可能性と課題"]
+# OSS参加者のみ
+ossdat = indat[indat[3] == "製薬業界におけるR活用の可能性と課題"]
+
+
+# 経験年数
+# 各水準の件数
+exyr = ossdat[4].value_counts()
+
+plt.title('統計解析業務に携わった経験年数を以下からご選択ください。')
+
+plt.pie(
+    exyr, 
+    labels=exyr.index, 
+    autopct=lambda p: f'{p:.1f}% ({p * sum(exyr) / 100:.0f})',
+    startangle=90
+)
+
+st.pyplot(plt)
