e68b9d5a623fe7901d9ed10dcde90c8915d4d919
Taku-Sakaue <98134385+Taku-Sakaue@users.noreply.github.com>
2026-06-10 16:32:08 +0900

    Update data_summary.py

--- data_summary.py
+++ data_summary.py
@@ -6,3 +6,9 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 
 st.title('参加者要約')
+
+# 参加者情報の読み取り
+indat = pd.read_excel(r'data/data.xlsx', skiprows=1, header=None)
+
+# OSS
+ossdat = indat[indat[6] == "製薬業界におけるR活用の可能性と課題"]
